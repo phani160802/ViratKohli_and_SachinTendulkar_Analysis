7a3507fdec01fd6a483a668005c6b9578a12358a
Phani Bhushan <56060884+phani160802@users.noreply.github.com>
2023-12-14 12:59:40 +0530

    Delete Boundaries.py

--- boundaries.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-def show_boundaries(virat_df,sachin_df):
-    
-    st.markdown("*** Let's Have a look at Boundaries Wise Analysis between Virat Kohli and Sachin Tendulkar***")
-    st.dataframe(virat_df)
-    st.dataframe(sachin_df)
